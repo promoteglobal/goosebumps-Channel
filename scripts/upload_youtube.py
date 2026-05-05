f587767a2d1ef6f311441eb44d37d2293b5c5293
promoteglobal <promoteglobal@gmail.com>
2026-05-05 20:03:39 +0800

    Update YouTube scope for playlist support

--- scripts/upload_youtube.py
+++ scripts/upload_youtube.py
@@ -165,7 +165,7 @@ def main():
         from google_auth_oauthlib.flow import InstalledAppFlow
         flow = InstalledAppFlow.from_client_secrets_file(
             "client_secrets.json",
-            scopes=["https://www.googleapis.com/auth/youtube.upload"]
+            scopes=["https://www.googleapis.com/auth/youtube"]
         )
         creds = flow.run_local_server(port=0)
         with open("youtube_token.json", "w") as f:
